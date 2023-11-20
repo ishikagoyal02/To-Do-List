072e9197f6cf88c44001c20ad469815b81748875
Ishika Goyal <goyalishika132@gmail.com>
2023-11-20 22:01:15 +0530

    improved the css

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const listitem = document.getElementById("type");
-//     const btnplus = document.getElementById("plus");
-//     const result = document.querySelector(".result.type");
-//     const dlt = document.querySelector(".plus.dlt");
-
-    
-
-//     btnplus.addEventListener('click', function () {
-//         result.innerText += listitem.value;
-
-//         const newbutton = document.createElement('button');
-//         newbutton.textContent = '-';
-//         newbutton.className = "minusbtn";
-//         result.appendChild(newbutton); // Append new button to the result container
-        
-//         //result.innerText += "\n";
-//     });
-
-//     dlt.addEventListener('click', function () {
-//         result.innerText = "";
-//     });
-
-//     newbutton.addEventListener('click', function () {
-//         result.innerText = "";
-//     });
-// });
 document.addEventListener('DOMContentLoaded', function () {
     const listitem = document.getElementById("type");
     const btnplus = document.getElementById("plus");
@@ -33,39 +6,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
     btnplus.addEventListener('click', function () {
         const task = listitem.value;
-        
-        // Create a new div to hold the task and its corresponding minus button
-        const taskDiv = document.createElement('div');
 
-        // Create a span for the task text
-        const taskSpan = document.createElement('span');
-        taskSpan.textContent = task;
+        if (task.trim() !== '') {
+            const taskDiv = document.createElement('div');
+            const newbutton = document.createElement('button');
+            newbutton.textContent = '-';
+            newbutton.className = "minusbtn";
 
-        // Create a minus button for the task
-        const newbutton = document.createElement('button');
-        newbutton.textContent = '-';
-        newbutton.className = "minusbtn";
+            taskDiv.textContent = task;
+            taskDiv.appendChild(newbutton);
 
-        // Append the task text and the minus button to the task div
-        taskDiv.appendChild(taskSpan);
-        taskDiv.appendChild(newbutton);
+            result.appendChild(taskDiv);
+            listitem.value = "";
 
-        // Append the task div to the result container
-        result.appendChild(taskDiv);
-
-        // Clear the input field after adding the task
-        listitem.value = "";
+            // Scroll to the bottom when a new task is added
+            result.scrollTop = result.scrollHeight;
+        }
     });
 
-    // Event listener to clear all tasks
     dlt.addEventListener('click', function () {
         result.innerHTML = "";
     });
 
-    // Event delegation for dynamically created minus buttons
     result.addEventListener('click', function (event) {
         if (event.target.classList.contains('minusbtn')) {
-            // Remove the corresponding task div when a minus button is clicked
             const taskDiv = event.target.parentNode;
             result.removeChild(taskDiv);
         }

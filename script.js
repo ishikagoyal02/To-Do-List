f787614f16c69479f9f0870663e19c5b57fdcdd5
Ishika Goyal <goyalishika132@gmail.com>
2023-11-21 17:37:44 +0530

    ui improved

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
     const listitem = document.getElementById("type");
     const btnplus = document.getElementById("plus");
-    const result = document.querySelector(".result.type");
+    //const result = document.querySelector(".result.type");
     const dlt = document.querySelector(".plus.dlt");
+    const m=document.querySelector(".m");
+    const result= document.createElement("div");
+    result.className="result";
+
+    var flag=0;
 
     btnplus.addEventListener('click', function () {
         const task = listitem.value;
+
 
         if (task.trim() !== '') {
             const taskDiv = document.createElement('div');
             const newbutton = document.createElement('button');
             newbutton.textContent = '-';
             newbutton.className = "minusbtn";
+            taskDiv.className="result type";
+
+            
 
             taskDiv.textContent = task;
             taskDiv.appendChild(newbutton);
 
             result.appendChild(taskDiv);
+
+            if(flag==0)
+            {
+                m.appendChild(result);
+                flag=1;
+            }
+            
             listitem.value = "";
 
             // Scroll to the bottom when a new task is added
